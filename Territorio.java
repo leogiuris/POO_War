bf87118b698c2809c448ab532f963aa2c5da8966
Antonio Machado Moura Jardim <48560795+Amjardim@users.noreply.github.com>
2020-05-28 19:36:24 -0300

    funcao playerAtaque introduzida, falta testar

---
+++ Territorio.java
@@ -0,0 +1,51 @@
+package MODEL;
+
+import java.util.*;
+
+import MODEL.Player.Cor;
+
+public class Territorio {
+	
+	public String nome;
+	public Continente continente;
+	public Cor cor;
+	public List<Exercito> exercitos = new ArrayList<Exercito>();
+	
+	public Territorio(String nome, Continente continente) {
+		this.nome = nome;
+		this.continente = continente;
+		continente.AddTerritorio(this);
+		BaralhoTerritorio.addCarta(this);
+	}
+	
+	public int getQtdExercitos() {
+		return exercitos.size();
+	}
+	
+	public Player.Cor getCor(){
+		if(exercitos.isEmpty()) {
+			return Player.Cor.vazio;
+		}
+		return exercitos.get(0).cor;
+	}
+	
+	public void setCor(Cor a){
+		cor =a;
+	}
+	
+	public boolean validaCor(Cor c) {
+		if(c == getCor() || getCor() == Cor.vazio)
+			return true;
+		else
+			return false;
+	}
+	
+	public boolean botarExercito(Exercito e) {	
+		if(validaCor(e.cor)){
+			exercitos.add(e);
+			return true;
+		}
+
+		return false;		
+	}
+}
